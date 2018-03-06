3a31d375012e8497d6f8926c3d763d0a938f7722
Sonix- <policeofficetw@gmail.com>
2018-03-06 15:10:31 +0100

    Fix js not working with turbolinks

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load',function(){
   $('.right.menu.open').on("click",function(e){
         e.preventDefault();
     $('.ui.vertical.menu').toggle();
